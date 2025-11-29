55f5f8a646e9a476d24f4769cdb7712cca8e13af
Vladimir Zahradnik <wexder19@gmail.com>
2025-11-29 12:59:53 +0100

    feat: add templates to flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,26 @@
 {
   outputs = _: {
-    templates =
-      {
-        go = {
-          description = "Development shell for go";
-          path = ./templates/go;
-        };
-        rust = {
-          description = "Development shell for rust";
-          path = ./templates/rust;
-        };
-        rust-pkg = {
-          description = "Development shell for rust package, with build";
-          path = ./templates/rust-pkg;
-        };
+    templates = {
+      go = {
+        description = "Development shell for go";
+        path = ./templates/go;
       };
+      rust = {
+        description = "Development shell for rust";
+        path = ./templates/rust;
+      };
+      rust-pkg = {
+        description = "Development shell for rust package, with build";
+        path = ./templates/rust-pkg;
+      };
+      flutter = {
+        description = "Development shell for flutter";
+        path = ./templates/flutter;
+      };
+      flutter-rust = {
+        description = "Development shell for flutter with rust packages";
+        path = ./templates/flutter-rust;
+      };
+    };
   };
 }

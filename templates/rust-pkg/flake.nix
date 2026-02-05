5793c57f7e76bbc5b85f1f5b16f5b3151d2287e4
Vladimir Zahradnik <wexder19@gmail.com>
2026-02-05 18:17:05 +0100

    feat: update rust templates

--- templates/rust-pkg/flake.nix
+++ templates/rust-pkg/flake.nix
@@ -13,17 +13,38 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, systems, make-shell, fenix, crane, ... }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      make-shell,
+      fenix,
+      crane,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ make-shell.flakeModules.default ];
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }:
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          openssl_pkgs = pkgs.openssl;
           fenixSys = fenix.packages.${system};
-          craneLib = (crane.mkLib pkgs).overrideToolchain
-            fenixSys.minimal.toolchain;
+          craneLib = (crane.mkLib pkgs).overrideToolchain fenixSys.minimal.toolchain;
           package = craneLib.buildPackage {
             src = ./.;
           };
@@ -36,7 +57,22 @@
           packages.default = package;
 
           make-shells.default = {
+            env = {
+              PKG_CONFIG_PATH = "${openssl_pkgs.dev}/lib/pkgconfig";
+              LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+              LD_LIBRARY_PATH = pkgs.lib.strings.concatStrings (
+                pkgs.lib.strings.intersperse ":" [
+                  "$LD_LIBRARY_PATH"
+                  "${openssl_pkgs.out}/lib"
+                  "${pkgs.stdenv.cc.cc.lib}/lib"
+                ]
+              );
+            };
+
             packages = [
+              pkgs.pkg-config
+              pkgs.openssl.dev
+
               (fenixSys.complete.withComponents [
                 "cargo"
                 "clippy"
@@ -50,4 +86,3 @@
         };
     };
 }
-
--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -9,18 +9,54 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, systems, make-shell, fenix, ... }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      make-shell,
+      fenix,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ make-shell.flakeModules.default ];
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }:
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
         let
           fenixSys = fenix.packages.${system};
+          openssl_pkgs = pkgs.openssl;
         in
         {
           make-shells.default = {
+            env = {
+              PKG_CONFIG_PATH = "${openssl_pkgs.dev}/lib/pkgconfig";
+              LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+              LD_LIBRARY_PATH = pkgs.lib.strings.concatStrings (
+                pkgs.lib.strings.intersperse ":" [
+                  "$LD_LIBRARY_PATH"
+                  "${openssl_pkgs.out}/lib"
+                  "${pkgs.stdenv.cc.cc.lib}/lib"
+                ]
+              );
+            };
+
             packages = [
+              pkgs.pkg-config
+              pkgs.openssl.dev
+
               (fenixSys.complete.withComponents [
                 "cargo"
                 "clippy"
@@ -34,4 +70,3 @@
         };
     };
 }
-
